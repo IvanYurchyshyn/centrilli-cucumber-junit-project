91d09eb85d3ff094744daaf35ea9562c345cb841
IvanYurchyshyn <yvanyay@gmail.com>
2023-07-11 12:37:53 -0500

    Created project

---
+++ src/test/java/com/centrilli/pages/BasePage.java
@@ -0,0 +1,10 @@
+package com.centrilli.pages;
+
+import com.centrilli.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
---
+++ src/test/java/com/centrilli/runners/Rerun.java
@@ -0,0 +1,4 @@
+package com.centrilli.runners;
+
+public class Rerun {
+}
