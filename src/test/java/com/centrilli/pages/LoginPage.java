11a7a3eeccc4cd0b9da584789bec21e371ecc59c
IvanYurchyshyn <yvanyay@gmail.com>
2023-07-13 08:58:38 -0500

    Added LoginPage

---
+++ src/test/java/com/centrilli/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package com.centrilli.pages;
+
+import com.centrilli.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "login")
+    public WebElement loginInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submitButton;
+
+
+
+}
