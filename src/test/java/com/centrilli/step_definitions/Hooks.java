160115be43f1dbac283a139fb1b9e946b7db3b66
IvanYurchyshyn <yvanyay@gmail.com>
2023-07-19 11:44:23 -0500

    Updated Hooks

--- src/test/java/com/centrilli/step_definitions/Hooks.java
+++ src/test/java/com/centrilli/step_definitions/Hooks.java
@@ -1,13 +1,18 @@
 package com.centrilli.step_definitions;
 
+import com.centrilli.pages.HomePage;
 import com.centrilli.utilities.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Hooks {
-
 
     @After
     public void teardownMethod(Scenario scenario){
