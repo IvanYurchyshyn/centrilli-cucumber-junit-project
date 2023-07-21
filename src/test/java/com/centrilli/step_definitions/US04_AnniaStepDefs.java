ddc783173b71bd742fc479c730727144cea16007
Annia_Cydeo <reyescuba@gmail.com>
2023-07-21 00:05:24 -0400

    Updating step definitions

--- src/test/java/com/centrilli/step_definitions/US04_AnniaStepDefs.java
+++ src/test/java/com/centrilli/step_definitions/US04_AnniaStepDefs.java
@@ -13,13 +13,11 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 public class US04_AnniaStepDefs {
-
     PosManagerCreateEventsPage_Annia posManagerCreateEventsPage_annia = new PosManagerCreateEventsPage_Annia();
 
 
     @Given("the calendar is open")
     public void the_calendar_is_open() {
-
         BrowserUtils.sleep(2);
         posManagerCreateEventsPage_annia.calendarModule.click();
     }
@@ -33,7 +31,6 @@ public class US04_AnniaStepDefs {
 
     @Then("I enter the event details")
     public void i_enter_the_event_details() {
-
         BrowserUtils.sleep(2);
         posManagerCreateEventsPage_annia.eventDetails();
     }
@@ -47,16 +44,6 @@ public class US04_AnniaStepDefs {
     @When("there is an existing event on the calendar")
     public void there_is_an_existing_event_on_the_calendar() {
 
-      /*  BrowserUtils.sleep(1);
-        WebElement existingEvent = Driver.getDriver().findElement(By.xpath("//div[@class='fc-content']//div[2]//div[1]"));
-
-        JavascriptExecutor js = ((JavascriptExecutor)Driver.getDriver());
-
-        BrowserUtils.sleep(3);
-
-        js.executeScript("arguments[0].scrollIntoView(true)",existingEvent);
-        */
-
         BrowserUtils.sleep(2);
 
         posManagerCreateEventsPage_annia.locatingEventCreatedBox.click();
@@ -68,37 +55,12 @@ public class US04_AnniaStepDefs {
 
         Assert.assertTrue(actualEventText.contains(expecteEventText));
 
-
     }
-
-
-   /* @And("I click on the event")
-    public void i_click_on_the_event() {
-*/
-       /* BrowserUtils.sleep(2);
-        posManagerCreateEventsPage_annia.locatingEventCreatedBox.click();
-
-        */
-       /* BrowserUtils.sleep(2);
-
-        WebElement eventCreated = Driver.getDriver().findElement(By.xpath("//div[@class='fc-content']//div[2]//div[1]"));
-
-        JavascriptExecutor js = ((JavascriptExecutor)Driver.getDriver());
-
-        BrowserUtils.sleep(3);
-
-        js.executeScript("arguments[0].scrollIntoView(true)",eventCreated);
-
-        posManagerCreateEventsPage_annia.eventCreated.click();
-   }
-*/
-
 
     @Then("the event details should be displayed")
     public void the_event_details_should_be_displayed() {
 
         posManagerCreateEventsPage_annia.eventCreated.isDisplayed();
-
 
     }
 
