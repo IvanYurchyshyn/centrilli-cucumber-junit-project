83b787c6f9997853bf6d5896e7dd3f50b16dd863
canany <66906676+canany@users.noreply.github.com>
2023-07-21 15:56:10 +0300

    Canan Us

--- src/test/java/com/centrilli/step_definitions/US05_CananStepDefs.java
+++ src/test/java/com/centrilli/step_definitions/US05_CananStepDefs.java
@@ -11,6 +11,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import javax.swing.*;
 
@@ -45,19 +46,14 @@ public class US05_CananStepDefs {
         BrowserUtils.sleep(5);
     }
 
-    @And("user fills the Name Input Box")
-    public void userFillsTheNameInputBox() {
-
-       // BrowserUtils.waitForClickablility(salesManagerModulePage.nameInputBox,5).click();
-
-        salesManagerModulePage.nameInputBox.sendKeys("Customer1");
-
+    @And("user fills the Name Input Box as {string}")
+    public void userFillsTheNameInputBoxAs(String customerName) {
+        salesManagerModulePage.nameInputBox.sendKeys(customerName);
     }
 
     @And("user clicks Save Button")
     public void userClicksSaveButton() {
         salesManagerModulePage.customersSaveButton.click();
-
     }
 
     @Then("user sees the information of customer which is created")
@@ -66,7 +62,7 @@ public class US05_CananStepDefs {
         boolean customerInfoDisplayed=salesManagerModulePage.customerForm.isDisplayed();
         Assert.assertTrue(customerInfoDisplayed);
 
-        String expectedCustomerInfo= "Customer1";
+        String expectedCustomerInfo= "John";
         Assert.assertEquals(expectedCustomerInfo,salesManagerModulePage.nameInfo.getText());
 
     }
@@ -81,7 +77,7 @@ public class US05_CananStepDefs {
     }
 
     @And("user clicks any customer")
-    public void userClicksAnyCustomer() { // I click only 1 customer ??
+    public void userClicksAnyCustomer() { // I click only 1 customer.
         salesManagerModulePage.customer2.click();
         BrowserUtils.sleep(5);
 
@@ -94,78 +90,124 @@ public class US05_CananStepDefs {
         BrowserUtils.sleep(5);
     }
 
-    @And("user selects Individual or Company radio button")
-    public void userSelectsIndividualOrCompanyRadioButton() {
-        salesManagerModulePage.individiualRadioButton.click();
+    @And("user selects Individual radio button")
+    public void userSelectsIndividualRadioButton() {
+        salesManagerModulePage.individualRadioButton.click();
+        Assert.assertTrue(salesManagerModulePage.individualRadioButton.isSelected());
+    }
+
+    @And("user selects Company radio button")
+    public void userSelectsCompanyRadioButton() {
         salesManagerModulePage.companyRadioButton.click();
         Assert.assertTrue(salesManagerModulePage.companyRadioButton.isSelected());
     }
 
-    @And("user edits Company")
-    public void userEditsCompany() {
-        salesManagerModulePage.companyDropdown.click();
-        BrowserUtils.sleep(2);
+
+    @And("user edits Name as {string}")
+    public void userEditsNameAs(String editName) {
+        salesManagerModulePage.nameInputBox.sendKeys (editName);
+        salesManagerModulePage.nameInputBox.click();
+      //  Assert.assertTrue((salesManagerModulePage.nameInputBox.getText().contains(editName)));
+
+    }
+
+    @And("user edits Company as {string}")
+    public void userEditsCompanyAs(String companyName) {
+        salesManagerModulePage.companyDropdownInputField.click();
+        BrowserUtils.sleep(1);
+        Select companySelect=new Select(salesManagerModulePage.companyDropdownInputField);
+        companySelect.selectByVisibleText(companyName);
+        String expectedCompanyName="ASfdADSF";
+        Assert.assertTrue(salesManagerModulePage.companyDropdownInputField.getText().contains(expectedCompanyName));
+
+    }
+
+    @And("user edits Street Input Boxes as {string},")
+    public void userEditsStreetInputBoxesAs(String street1) {
+        salesManagerModulePage.streetInputBox.sendKeys(street1);
+        String expectedStreet1="Oe Street";
+        Assert.assertTrue(salesManagerModulePage.streetInputBox.getText().contains(expectedStreet1));
+
+    }
+
+    @And("user edits Streetb Input Boxes as {string},")
+    public void userEditsStreetbInputBoxesAs(String street2) {
+        salesManagerModulePage.street2InputBox.sendKeys(street2);
+        String expectedStreet2="1212";
+        Assert.assertTrue(salesManagerModulePage.street2InputBox.getText().contains(expectedStreet2));
+
+    }
+
+    @And("user edits City as {string}")
+    public void userEditsCityAs(String city) {
+        salesManagerModulePage.cityInputBox.sendKeys(city);
+        String expectedCity="London";
+        Assert.assertTrue(salesManagerModulePage.cityInputBox.getText().contains(expectedCity));
+    }
+
+
+    @And("user edits Zip as {string}")
+    public void userEditsZipAs(String zip) {
+        salesManagerModulePage.zipInputBox.sendKeys(zip);
+        String expectedZip="N1P 3AG";
+        Assert.assertTrue(salesManagerModulePage.zipInputBox.getText().contains(expectedZip));
+
+    }
+
+    @And("user edits TIN as {string}")
+    public void userEditsTINAs(String tin) {
+        salesManagerModulePage.tinInputBox.sendKeys(tin);
+        String expectedTin="2544523";
+        Assert.assertTrue(salesManagerModulePage.tinInputBox.getText().contains(expectedTin));
+    }
+
+    @And("user edits Job Position as {string}")
+    public void userEditsJobPositionAs(String job) {
+        salesManagerModulePage.jobPositionInputBox.sendKeys(job);
+        String expectedJob="Consultant";
+        Assert.assertTrue(salesManagerModulePage.jobPositionInputBox.getText().contains(expectedJob));
 
 
     }
 
-    @And("user edits Street Input Boxes")
-    public void userEditsStreetInputBoxes() {
+    @And("user edits Phone as {string}")
+    public void userEditsPhoneAs(String phone) {
+
+        salesManagerModulePage.phoneInputBox.sendKeys(phone);
+        String expectedPhone="440903022";
+        Assert.assertTrue(salesManagerModulePage.phoneInputBox.getText().contains(expectedPhone));
     }
 
-    @And("user edits City")
-    public void userEditsCity() {
+    @And("user edits Mobile as {string}")
+    public void userEditsMobileAs(String mobile) {
+        salesManagerModulePage.mobileInputBox.sendKeys(mobile);
+        String expectedMobile="255542321";
+        Assert.assertTrue(salesManagerModulePage.mobileInputBox.getText().contains(expectedMobile));
+
     }
 
-    @And("user edits State")
-    public void userEditsState() {
+    @And("user edits Email as {string}")
+    public void userEditsEmailAs(String email) {
+
+        salesManagerModulePage.mailIputBox.sendKeys(email);
+        String expectedMail="jnf@gmail.com";
+        Assert.assertTrue(salesManagerModulePage.mailIputBox.getText().contains(expectedMail));
+
     }
 
-    @And("user edits Country")
-    public void userEditsCountry() {
+    @And("user edits Website as {string}")
+    public void userEditsWebsiteAs(String website) {
+        salesManagerModulePage.websiteInputBox.sendKeys(website);
+        String expectedWebsite="jnf@aa.io.com";
+        Assert.assertTrue(salesManagerModulePage.websiteInputBox.getText().contains(expectedWebsite));
+
     }
 
-    @And("user edits TIN")
-    public void userEditsTIN() {
-    }
 
-    @And("User edits Tags")
-    public void userEditsTags() {
-    }
-
-    @And("user edits Job Position")
-    public void userEditsJobPosition() {
-    }
-
-    @And("user edits Phone")
-    public void userEditsPhone() {
-    }
-
-    @And("user edits Mobile")
-    public void userEditsMobile() {
-    }
-
-    @And("user edits Email")
-    public void userEditsEmail() {
-    }
-
-    @And("user edits Website")
-    public void userEditsWebsite() {
-    }
-
-    @And("user edits Title")
-    public void userEditsTitle() {
-    }
-
-    @And("user edits Language")
-    public void userEditsLanguage() {
-    }
-
-    @And("user clicks Save")
+    @Then("user clicks Save")
     public void userClicksSave() {
+        salesManagerModulePage.editSaveButton.click();
     }
 
-    @Then("all the boxes should be edited")
-    public void allTheBoxesShouldBeEdited() {
-    }
+
 }
--- src/test/resources/features/US05_Canan.feature
+++ src/test/resources/features/US05_Canan.feature
@@ -10,30 +10,27 @@ Feature: As a Sales Manager, I should be able to create a new customer from "Sal
 		When user clicks Sales Button at menu
 		And user clicks Customers Button at sub-menu
 		And user clicks Create Button
-		And user fills the Name Input Box
+		And user fills the Name Input Box as "John"
 		And user clicks Save Button
 		Then user sees the information of customer which is created
-
 
 	@B29G29-239
 	Scenario: Verify sales Manager can edit customer information
 		Given user is on Customers page
 		When user clicks any customer
 		When user clicks Edit Button
-		And user selects Individual or Company radio button
-		And user edits Company
-		And user edits Street Input Boxes
-		And user edits City
-		And user edits State
-		And user edits Country
-		And user edits TIN
-		And User edits Tags
-		And user edits Job Position
-		And user edits Phone
-		And user edits Mobile
-		And user edits Email
-		And user edits Website
-		And user edits Title
-		And user edits Language
-		And user clicks Save
-		Then all the boxes should be edited
+		And user selects Individual radio button
+		And user selects Company radio button
+		And user edits Name as "3"
+		And user edits Street Input Boxes as "Oe Street",
+		And user edits Streetb Input Boxes as "1212",
+		And user edits City as "London"
+		And user edits Zip as "N1P 3AG"
+		And user edits TIN as "2544523"
+		And user edits Job Position as "Consultant"
+		And user edits Phone as "440903022"
+		And user edits Mobile as "255542321"
+		And user edits Email as "jnf@gmail.com"
+		And user edits Website as "aa.io.com"
+		And user edits Company as "ASfdADSF"
+		Then user clicks Save
